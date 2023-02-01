c2669ff19c547c90eb2e5ea8c1c185c093eb9193
PUSHRSP <pushrsp@gmail.com>
2023-02-01 15:09:15 +0900

    member 필드 추가

--- src/main/java/com/example/practicespringdatajpa/entity/Member.java
+++ src/main/java/com/example/practicespringdatajpa/entity/Member.java
@@ -1,25 +1,44 @@
 package com.example.practicespringdatajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
-    private String username;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
-    protected Member() {
-    }
+    private int age;
+    private String username;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+
+        if (team != null)
+            changeTeam(team);
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 
 }
