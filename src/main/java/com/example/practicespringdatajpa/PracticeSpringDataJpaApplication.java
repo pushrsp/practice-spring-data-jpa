900b8c34fad2b282c6afccf233696b9ff4e67109
PUSHRSP <pushrsp@gmail.com>
2023-02-01 13:53:01 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/practicespringdatajpa/PracticeSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicespringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringDataJpaApplication.class, args);
+    }
+
+}
