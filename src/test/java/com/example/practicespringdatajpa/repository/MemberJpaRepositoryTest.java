8d383d1f2bfd0e9c10771fd188ad6ca2435b47f4
PUSHRSP <pushrsp@gmail.com>
2023-02-01 15:09:04 +0900

    member 기능 추가

--- src/test/java/com/example/practicespringdatajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/example/practicespringdatajpa/repository/MemberJpaRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -28,6 +30,31 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD() throws Exception {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //when
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        List<Member> members = memberJpaRepository.findAll();
+        Long count = memberJpaRepository.count();
+
+        //then
+        assertThat(member1).isEqualTo(findMember1);
+        assertThat(member2).isEqualTo(findMember2);
+
+
+        assertThat(members.size()).isEqualTo(2);
+        assertThat(count).isEqualTo(2);
     }
 
 
