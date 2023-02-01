f46de5f87a5647c9a4bc791c13354c8d0cf94b3e
PUSHRSP <pushrsp@gmail.com>
2023-02-01 14:30:38 +0900

    spring data jpa 사용

---
+++ src/test/java/com/example/practicespringdatajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.practicespringdatajpa.repository;
+
+import com.example.practicespringdatajpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member("memberA");
+        Member saveMember = memberRepository.save(member);
+
+        //when
+        Member findMember = memberRepository.findById(saveMember.getId()).get();
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
