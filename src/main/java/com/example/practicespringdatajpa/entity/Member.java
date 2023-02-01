4378f52a38596e158f5d0f7a8782d0a6addc32d5
PUSHRSP <pushrsp@gmail.com>
2023-02-01 14:30:18 +0900

    member 엔티티 추가

---
+++ src/main/java/com/example/practicespringdatajpa/entity/Member.java
@@ -0,0 +1,25 @@
+package com.example.practicespringdatajpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    protected Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+}
